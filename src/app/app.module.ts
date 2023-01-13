2c250f487189853f5f7f2679aee6696ce4cd8d60
schneider <marydorcassinaida54@gmail.com>
2023-01-13 03:02:07 +0300

    work in progress

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { GetAQuoteComponent } from './components/cart/get-a-quote/get-a-quote.component';
+import { ProductListingComponent } from './components/marketplace/product-listing/product-listing.component';
+import { ProductDetailsComponent } from './components/marketplace/product-details/product-details.component';
+import { TransactionHistoryComponent } from './components/transactions/transaction-history/transaction-history.component';
+import { TransactionStatusComponent } from './components/transactions/transaction-status/transaction-status.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    GetAQuoteComponent,
+    ProductListingComponent,
+    ProductDetailsComponent,
+    TransactionHistoryComponent,
+    TransactionStatusComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/transactions/transaction-status/transaction-status.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-status',
+  templateUrl: './transaction-status.component.html',
+  styleUrls: ['./transaction-status.component.css']
+})
+export class TransactionStatusComponent {
+
+}
